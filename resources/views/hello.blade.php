ecb2c533477e4f8afbbb96e5c756a403a00f0783
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-08-24 12:01:31 +0700

    menambahkan breadcrumb

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -1,8 +1,16 @@
 @extends('layouts.main')
+
+@section('breadcrumb')
+    <li class="breadcrumb-item">
+        <a href="#">Home</a>
+    </li>
+    <li class="breadcrumb-item active">Hello</li>
+@endsection
+
 @section('content')
 <div class="col-md-12">
     <h2>Sample Page</h2>
     <p>Hello {{ $nama }} dan alamat anda {{ isset($alamat)?$alamat:'belum diisi' }}</p>
 </div>
-    
+
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -225,10 +225,7 @@
 
         <!-- Breadcrumbs -->
         <ol class="breadcrumb">
-          <li class="breadcrumb-item">
-            <a href="#">Dashboard</a>
-          </li>
-          <li class="breadcrumb-item active">My Dashboard</li>
+          @yield('breadcrumb')
         </ol>
 
         <!-- Icon Cards -->
