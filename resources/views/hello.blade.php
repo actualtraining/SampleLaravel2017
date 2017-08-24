abb338633ee75726bf6ed1b3b09a0db57268490f
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-08-24 11:51:34 +0700

    menambahkan layout

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.main')
-@section('main')
+@section('content')
+<div class="col-md-12">
     <h2>Sample Page</h2>
     <p>Hello {{ $nama }} dan alamat anda {{ isset($alamat)?$alamat:'belum diisi' }}</p>
+</div>
+    
 @endsection
